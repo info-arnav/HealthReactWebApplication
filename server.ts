2a2360477bdc5c4b50758fe0f16a1abccfbc8d9c
Arnav S Gupta <info@arnavgupta.net>
2020-11-03 00:20:17 +0530

    csvig

--- server.ts
+++ server.ts
@@ -28,7 +28,11 @@ app.listen(serverPort, (req, res) => {});
 
 //*use
 app.use(cors());
-app.use(express.static(path.join(__dirname, "./build")));
+app.use(
+  express.static(
+    path.join(__dirname, "./appChallengwe/HealthReactWebApplication/build")
+  )
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileupload());
@@ -309,5 +313,11 @@ app.get("/privacy", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./build", "index.html"));
+  res.sendFile(
+    path.join(
+      __dirname,
+      "./appChallengwe/HealthReactWebApplication/build",
+      "index.html"
+    )
+  );
 });
